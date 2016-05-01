f36f652aa8a76cb27136a18fd07366dd5651bdde
Gabor Csardi <csardi.gabor@gmail.com>
2016-05-02 00:49:14 +0200

    Mason fixup

--- R/package.R
+++ R/package.R
@@ -1,8 +1,8 @@
 
 #' Manipulation of 'ANSI' colored strings
 #'
-#' String manipulation functions for 'ANSI' colored strings. Creates substrings,
-  split strings, wraps strings, and keeps the coloring proper.
+#' String manipulation functions for 'ANSI' colored strings. Creates
+#' substrings, split strings, wraps strings, and keeps the coloring proper.
 #'
 #' @docType package
 #' @name ansistrings
