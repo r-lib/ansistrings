195a4133584800258cd9b59df4d4159ebb1ab8f5
Gábor Csárdi <csardi.gabor@gmail.com>
2017-03-03 13:13:58 +0000

    Rewrite make_ansi_map1 in C

--- R/map.R
+++ R/map.R
@@ -81,65 +81,9 @@ map_ansi_to_raw1 <- function(map, ansi) {
 make_ansi_map1 <- function(str) {
   re <- re_exec_all(str, re_ansi())
   shifts <- make_shifts1(re)
-
-  ## Number of ANSI tags
-  num_tags <- length(re$.match$start[[1]])
-  if (num_tags == 0) {
-    return(list(
-      map = data.frame(
-        stringsAsFactors = FALSE,
-        start = numeric(),
-        end = numeric(),
-        open = character(),
-        close = character()
-      ),
-      shifts = shifts
-    ))
-  }
-
-  ## Which tags are start tags / end tags
-  start_tags <- re$start$start[[1]] > 0
-
-  res <- data.frame(
-    stringsAsFactors = FALSE,
-    start = shifts[, 1],
-    end = rep(NA, num_tags),
-    open = re$.match$match[[1]],
-    close = rep(NA_character_, num_tags)
-  )
-
-  ## To avoid multiple calls to this
-  re_closes <- re_closes()
-
-  ## Currently active start tags
-  active <- numeric()
-
-  for (i in seq_len(num_tags)) {
-    ## If it is a start tag, then we mark it as active
-    if (start_tags[i]) {
-      active <- c(active, i)
-
-    ## End tag, check which active start tags it closes
-    } else {
-      endcode <- re$end$match[[1]][i]
-      endtag <- re$.match$match[[1]][i]
-      active_start_codes <- re$start$match[[1]][active]
-      closes <- grepl(re_closes[[endcode]], active_start_codes)
-      res$end[active[closes]] <- shifts[i, 1] - 1
-      res$close[active[closes]] <- endtag
-      active <- setdiff(active, active[closes])
-    }
-  }
-
-  ## We only really need one row per start tag
-  res <- res[start_tags, ]
-  rownames(res) <- NULL
-
-  ## Close at the end of the string, if the ANSI close tag is missing
-  res$end[is.na(res$end)] <- nchar(str) - tail(shifts[,1], 1)
-  res$close[is.na(res$close)] <- ""
-
-  list(map = res, shifts = shifts)
+  .Call("ansistrings_make_ansi_map1", str, re$.match$start[[1]],
+        re$.match$end[[1]], re$.match$match[[1]], re$start$start[[1]],
+        re$start$match[[1]], re$end$match[[1]], PACKAGE = "ansistrings")
 }
 
 #' @rdname make_ansi_map1
--- src/map.c
+++ src/map.c
@@ -10,7 +10,7 @@ SEXP ansistrings_make_shifts1(SEXP start, SEXP end) {
   INTEGER(dim)[0] = n;
   INTEGER(dim)[1] = 3;
   setAttrib(result, R_DimSymbol, dim);
-  
+
   for (i = 0; i < n; i++) {
     INTEGER(result)[n + i] = INTEGER(start)[i];
     INTEGER(result)[2 * n + i] = INTEGER(end)[i] - INTEGER(start)[i] + 1;
@@ -25,7 +25,7 @@ SEXP ansistrings_make_shifts1(SEXP start, SEXP end) {
     INTEGER(result)[i] =
       INTEGER(result)[n + i] - INTEGER(result)[2 * n + i - 1];
   }
-  
+
   UNPROTECT(2);
   return result;
 }
@@ -49,3 +49,149 @@ SEXP ansistrings_map_ansi_to_raw1(SEXP shifts, SEXP ansi) {
 
   return ScalarInteger(i == 0 ? cansi : cansi - INTEGER(shifts)[2 * n + i - 1]);
 }
+
+/* Does this `endcode` close this `startcode`? */
+
+int ansistrings_i_re_closes(const char *startcode, const char *endcode) {
+
+  if (endcode[0] == '3' && endcode[1] == '9') {
+    return startcode[0] == '3' &&
+      '0' <= startcode[1] && startcode[1] <= '8';
+
+  } else if (endcode[0] == '4' && endcode[1] == '9') {
+    return startcode[0] == '4' &&
+      '0' <= startcode[1] && startcode[1] <= '8';
+
+  } else if (endcode[0] == '2' && endcode[1] == '2') {
+    return (startcode[0] == '1' || startcode[0] == '2') &&
+      startcode[1] == '\0';
+
+  } else if (endcode[0] == '2' && endcode[1] == '3') {
+    return startcode[0] == '3' && startcode[1] == '\0';
+
+  } else if (endcode[0] == '2' && endcode[1] == '4') {
+    return startcode[0] == '4' && startcode[1] == '\0';
+
+  } else if (endcode[0] == '2' && endcode[1] == '7') {
+    return startcode[0] == '7' && startcode[1] == '\0';
+
+  } else if (endcode[0] == '2' && endcode[1] == '8') {
+    return startcode[0] == '8' && startcode[1] == '\0';
+
+  } else if (endcode[0] == '2' && endcode[1] == '9') {
+    return startcode[0] == '9' && startcode[1] == '\0';
+
+  } else if (endcode[0] == '0') {
+    return 1;
+  }
+
+  /* Unknown end tag */
+  return 0;
+}
+
+SEXP ansistrings_make_ansi_map1(SEXP str, SEXP re_match_start,
+				SEXP re_match_end, SEXP re_match_match,
+				SEXP re_start_start, SEXP re_start_match,
+				SEXP re_end_match) {
+
+  int *pre_start_start = INTEGER(re_start_start);
+  SEXP names, result = PROTECT(allocVector(VECSXP, 2));
+  SEXP shifts =
+    PROTECT(ansistrings_make_shifts1(re_match_start, re_match_end));
+
+  SEXP map, rownames, res_names;
+  int num_tags = LENGTH(re_match_start);
+  int *active = (int *) R_alloc(num_tags, sizeof(int));
+  int *mapmap = (int *) R_alloc(num_tags, sizeof(int));
+  int activeptr = 0;
+  int i, p;
+
+#define is_start_tag(i) (pre_start_start[(i)] > 0)
+#define is_end_tag(i)   (pre_start_start[(i)] <= 0)
+  int num_start_tags = 0;
+
+  for (i = 0; i < num_tags; i++) num_start_tags += is_start_tag(i);
+
+  map = PROTECT(allocVector(VECSXP, 4));
+  SET_VECTOR_ELT(map, 0, allocVector(INTSXP, num_start_tags));
+  SET_VECTOR_ELT(map, 1, allocVector(INTSXP, num_start_tags));
+  SET_VECTOR_ELT(map, 2, allocVector(STRSXP, num_start_tags));
+  SET_VECTOR_ELT(map, 3, allocVector(STRSXP, num_start_tags));
+  names = PROTECT(allocVector(STRSXP, 4));
+  SET_STRING_ELT(names, 0, mkChar("start"));
+  SET_STRING_ELT(names, 1, mkChar("end"));
+  SET_STRING_ELT(names, 2, mkChar("open"));
+  SET_STRING_ELT(names, 3, mkChar("close"));
+  rownames = PROTECT(allocVector(INTSXP, 2));
+  INTEGER(rownames)[0] = NA_INTEGER;
+  INTEGER(rownames)[1] = -num_start_tags;
+  setAttrib(map, R_NamesSymbol, names);
+  setAttrib(map, R_RowNamesSymbol, rownames);
+  setAttrib(map, R_ClassSymbol, ScalarString(mkChar("data.frame")));
+  UNPROTECT(2);  		/* names, rownames */
+
+  PROTECT(res_names = allocVector(STRSXP, 2));
+  SET_STRING_ELT(res_names, 0, mkChar("map"));
+  SET_STRING_ELT(res_names, 1, mkChar("shifts"));
+  setAttrib(result, R_NamesSymbol, res_names);
+  UNPROTECT(1);			/* res_names */
+
+  /* These will be modified in place, so we can put them in now */
+  SET_VECTOR_ELT(result, 0, map);
+  SET_VECTOR_ELT(result, 1, shifts);
+  UNPROTECT(2);			/* map, shifts */
+
+  /* Is there anything to do? */
+  if (num_start_tags == 0) {
+    UNPROTECT(1);		/* result */
+    return result;
+  }
+
+  for (i = 0, p = 0 ; i < num_tags; i++) {
+    if (is_start_tag(i)) {
+      active[activeptr++] = i;
+      mapmap[i] = p;
+      INTEGER(VECTOR_ELT(map, 0))[p] = INTEGER(shifts)[i];
+      INTEGER(VECTOR_ELT(map, 1))[p] = 0; /* instead of memset(0) */
+      SET_STRING_ELT(VECTOR_ELT(map, 2), p, STRING_ELT(re_match_match, i));
+      p++;
+
+    } else {
+      const char *endcode = CHAR(STRING_ELT(re_end_match, i));
+      const char *endtag = CHAR(STRING_ELT(re_match_match, i));
+
+      /* Need to go over the active tags, to see which one is closed by
+	 this end tag. */
+      int j;
+      for (j = 0; j < activeptr; ) {
+	int a = active[j];
+	int m = mapmap[a];
+	const char *startcode = CHAR(STRING_ELT(re_start_match, a));
+	int closes = ansistrings_i_re_closes(startcode, endcode);
+	if (closes) {
+	  INTEGER(VECTOR_ELT(map, 1))[m] = INTEGER(shifts)[i] - 1;
+	  SET_STRING_ELT(VECTOR_ELT(map, 3), m, mkChar(endtag));
+	  active[j] = active[activeptr];
+	  activeptr--;
+	} else {
+	  j++;
+	}
+      }
+    }
+  }
+
+  /* Close at the end of the string, if the ANSI close tag is missing */
+  p = strlen(CHAR(STRING_ELT(str, 0))) - INTEGER(shifts)[num_tags - 1];
+  for (i = 0; i < num_start_tags; i++) {
+    if (INTEGER(VECTOR_ELT(map, 1))[i] == 0) {
+      INTEGER(VECTOR_ELT(map, 1))[i] = p;
+      SET_STRING_ELT(VECTOR_ELT(map, 3), i, mkChar(""));
+    }
+  }
+
+  UNPROTECT(1);			/* result */
+  return result;
+}
+
+#undef is_start_tag
+#undef is_end_tag
