c8afad7442799c0ec456865c87d744bf39c33aab
Gábor Csárdi <csardi.gabor@gmail.com>
2017-03-08 09:46:07 +0000

    Fix color regexes, issue #5

---
+++ tests/testthat/test-regex.R
@@ -0,0 +1,16 @@
+
+context("ANSI regexes")
+
+test_that("Issue #5", {
+  yy <- "\033[48;5;114mhello world\033[49m"
+  expect_equal(
+    gsub(re_ansi(), "", yy, perl = TRUE),
+    "hello world"
+  )
+
+  zz <- '\033[31;47mHello\033[0m\n'
+  re <- rematch2::re_match_all(zz, re_ansi())
+  skip("Multiple commands in one tag are not supported yet")
+  expect_equal(re$start, list("31;47"))
+  expect_equal(re$end, list("0"))
+})
