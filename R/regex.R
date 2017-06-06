425bc954aea3739da8829f71af6064579ca289f8
Gábor Csárdi <csardi.gabor@gmail.com>
2017-06-06 09:25:55 -0700

    Fix up regexes with and w/o capture groups

--- R/regex.R
+++ R/regex.R
@@ -1,29 +1,54 @@
 
 re_255           <- "(?:[01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])"
 
-re_color_8       <- "(?<color_8>3[0-7])"
-re_bgcolor_8     <- "(?<bgcolor_8>4[0-7])"
+re_color_8       <- "3[0-7]"
+re_g_color_8     <- sprintf("(?<color_8>%s)", re_color_8)
 
-re_color_256     <- sprintf("(?<color_256>38;5;%s)", re_255)
-re_bgcolor_256   <- sprintf("(?<bgcolor_256>48;5;%s)", re_255)
+re_bgcolor_8     <- "4[0-7]"
+re_g_bgcolor_8   <- sprintf("(?<bgcolor_8>%s)", re_bgcolor_8)
 
-re_color_rgb     <- sprintf("(?<color_rgb>38;2;%s;%s;%s)", re_255,
-                            re_255, re_255)
-re_bgcolor_rgb   <- sprintf("(?<bgcolor_rgb>48;2;%s;%s;%s)", re_255,
-                            re_255, re_255)
+re_color_256     <- sprintf("38;5;%s", re_255)
+re_g_color_256   <- sprintf("(?<color_256>%s)", re_color_256)
+
+re_bgcolor_256   <- sprintf("48;5;%s", re_255)
+re_g_bgcolor_256 <- sprintf("(?<bgcolor_256>%s)", re_bgcolor_256)
+
+re_color_rgb     <- sprintf("38;2;%s;%s;%s", re_255, re_255, re_255)
+re_g_color_rgb   <- sprintf("(?<color_rgb>%s)", re_color_rgb)
+
+re_bgcolor_rgb   <- sprintf("48;2;%s;%s;%s", re_255, re_255, re_255)
+re_g_bgcolor_rgb <- sprintf("(?<bgcolor_rgb>%s)", re_bgcolor_rgb)
 
 re_color         <- sprintf("(?:%s|%s|%s)", re_color_8, re_color_256,
                             re_color_rgb)
+re_g_color       <- sprintf("(?:%s|%s|%s)", re_g_color_8, re_g_color_256,
+                            re_g_color_rgb)
+
 re_bgcolor       <- sprintf("(?:%s|%s|%s)", re_bgcolor_8, re_bgcolor_256,
                             re_bgcolor_rgb)
+re_g_bgcolor     <- sprintf("(?:%s|%s|%s)", re_g_bgcolor_8,
+                            re_g_bgcolor_256, re_g_bgcolor_rgb)
 
-re_bold          <- "(?<bold>1)"
-re_blurred       <- "(?<blurred>2)"
-re_italic        <- "(?<italic>3)"
-re_underline     <- "(?<underline>4)"
-re_inverse       <- "(?<inverse>7)"
-re_hidden        <- "(?<hidden>8)"
-re_strikethrough <- "(?<strikethrough>9)"
+re_bold          <- "1"
+re_g_bold        <- "(?<bold>1)"
+
+re_blurred       <- "2"
+re_g_blurred     <- "(?<blurred>2)"
+
+re_italic        <- "3"
+re_g_italic      <- "(?<italic>3)"
+
+re_underline     <- "4"
+re_g_underline   <- "(?<underline>4)"
+
+re_inverse       <- "7"
+re_g_inverse     <- "(?<inverse>7)"
+
+re_hidden        <- "8"
+re_g_hidden      <- "(?<hidden>8)"
+
+re_strikethrough   <- "9"
+re_g_strikethrough <- "(?<strikethrough>9)"
 
 re_endcolor         <- "39"
 re_endbgcolor       <- "49"
@@ -46,15 +71,20 @@ re_endreset         <- "0"
 
 re_ansi <- function(groups = FALSE) {
 
-  ## Removed the named capture groups from a regular expression,
-  ## to speed up matching and constructing the result object
-  no_groups <- function(x) gsub("\\?<[a-zA-Z_]+>", "", x)
+  if (groups) {
+    re_start <- paste(
+      sep = "|",
+      re_g_color, re_g_bgcolor, re_g_bold, re_g_blurred, re_g_italic,
+      re_g_underline, re_g_inverse, re_g_hidden, re_g_strikethrough
+    )
 
-  re_start <- paste(
-    sep = "|",
-    re_color, re_bgcolor, re_bold, re_blurred, re_italic, re_underline,
-    re_inverse, re_hidden, re_strikethrough
-  )
+  } else {
+    re_start <- paste(
+      sep = "|",
+      re_color, re_bgcolor, re_bold, re_blurred, re_italic, re_underline,
+      re_inverse, re_hidden, re_strikethrough
+    )
+  }
 
   re_end <- paste(
     sep = "|",
@@ -62,11 +92,6 @@ re_ansi <- function(groups = FALSE) {
     re_endunderline, re_endinverse, re_endhidden, re_endstrikethrough,
     re_endreset
   )
-
-  if (!groups) {
-    re_start <- no_groups(re_start)
-    re_end   <- no_groups(re_end)
-  }
 
   paste0(
     "\\x{001b}\\[",
